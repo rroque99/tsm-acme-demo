18cff75ea083341f29073a2261283b3c5c06eaa1
rotbau <37818713+rotbau@users.noreply.github.com>
2023-06-21 14:23:06 -0400

    Update tsm-onboard-cluster.sh

--- onboarding/tsm-onboard-cluster.sh
+++ onboarding/tsm-onboard-cluster.sh
@@ -14,43 +14,58 @@ set -e
 # Export User API Token and Cluster Name
 # export TSM_TOKEN="redacted"
 # export TSM_URL="https://prod-2.nsxservicemesh.vmware.com"
-echo $TSM_TOKEN
+echo $CSP_TOKEN
 echo $TSM_URL
 export CLUSTER_NAME=$1
+
+# Exhchange CSP API Token for Access Token
 echo "Exchanging API token for access token"
 AT=$(curl -s 'https://console.cloud.vmware.com/csp/gateway/am/api/auth/api-tokens/authorize' \
             -H 'accept: application/json, text/plain, */*' \
-            --data-raw "refresh_token=${TSM_TOKEN}" --compressed | jq -r '.access_token')
+            --data-raw "refresh_token=${CSP_TOKEN}" --compressed | jq -r '.access_token')
 echo $AT
+
+# Create Cluster Objects and Retrieve Token for Secret
+echo "Creating Cluster Object and Getting TSM Token for Secret"
+TOKEN=$(curl -X PUT "https://$TSM_URL/tsm/v1alpha2/projects/default/clusters/${CLUSTER_NAME}?createOnly=true" \
+                   -H "csp-auth-token:$AT" \
+                   -H 'content-type: application/json' \
+                   -d '{"displayName": "'"${CLUSTER_NAME}"'", "description":"Test cluster", "tags":["msp-dc", "vsphere"], "labels":[{"key":"Proxy Location", "value":"aviproxybb"}], "autoInstallServiceMesh":true, "enableNamespaceInclusions":true, "enableInternalGateway":false, "namespaceInclusions":[{"type": "EXACT", "match": "acme"}], "autoInstallServiceMeshConfig":{"restrictDefaultExternalAccess":false}}' \
+                   | jq -r '.token')
+
+# Sleep to allow cluster object to get fully created before getting onboard URL
+sleep 2
+
+# Get Onboarding URL for Cluster
 echo "Fetching Onboarding URL"
-ONBOARD_URL=$(curl https://$TSM_URL/tsm/v1alpha1/clusters/onboard-url \
+# old v1alpah1 call
+
+ONBOARD_URL=$(curl https://$TSM_URL/tsm/v1alpha2/projects/default/clusters/$CLUSTER_NAME/onboarding-url \
                   -H "csp-auth-token:$AT" | jq -r '.url')
 
-# Validate kubectl context is correct
+# Validate kubectl context is correct before applying onboarding manifest
 echo --------------------------
 kubectl config current-context
 echo --------------------------
 read -p "Is the correct Kubernetes context selected (y/N)"  -n 1 -r
 echo
 
-# Install TSM Components
+# Apply Onboardng Manifest
 kubectl apply -f $ONBOARD_URL
-echo "Getting TSM Token for Secret"
-TOKEN=$(curl -X PUT "https://$TSM_URL/tsm/v1alpha1/clusters/${CLUSTER_NAME}?createOnly=true" \
-                   -H "csp-auth-token:$AT" \
-                   -H 'content-type: application/json' \
-                   -d '{"displayName": "'"${CLUSTER_NAME}"'", "description":"Test TKG cluster", "tags":["msp-dc", "vsphere"], "labels":[{"key":"Proxy Location", "value":"aviproxy"}], "autoInstallServiceMesh":true, "enableNamespaceExclusions":true, "enableInternalGateway":true, "namespaceExclusions":[{"type": "EXACT", "match": "vmware-system-tsm"}, {"type": "EXACT", "match": "vmware-system-tmc"}], "autoInstallServiceMeshConfig":{"restrictDefaultExternalAccess":false}}' \
-                   | jq -r '.token')
+
+# Create Secret from Token
 echo "Creating Secret from TSM Token"
 kubectl -n vmware-system-tsm create secret generic cluster-token --from-literal=token=$TOKEN
+
+# Loop Through Cluster State
 echo "Setting initial state to init"
 STATE="INIT"
 sleep 60
 until [[ $STATE == "Ready" ]]
 do
     echo "${CLUSTER_NAME} State = ${STATE}, waiting..."
-    sleep 15
-    STATE=$(curl -X GET "https://$TSM_URL/tsm/v1alpha1/clusters/$CLUSTER_NAME" \
+    sleep 5
+    STATE=$(curl -X GET "https://$TSM_URL/tsm/v1alpha2/projects/default/clusters/$CLUSTER_NAME" \
                    -H "Accept: application/json, text/plain, */*" \
                    -H "csp-auth-token: $AT" | jq -r '.status.state')
 done
